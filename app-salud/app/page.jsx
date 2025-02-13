1085e9da331df2f3d159fc62c35527814f69c4fa
Leysa Pozo <leysapozo41@gmail.com>
2025-02-13 10:13:54 -0300

    agregando navbar y con algunos estilos

---
+++ app-salud/app/page.jsx
@@ -0,0 +1,16 @@
+
+import Link from 'next/link';
+
+export default function HomePage() {
+    return (
+        <main className="main flex items-center justify-center md:h-screen">
+            <div className="div-start relative mx-auto flex w-full max-w-[400px] flex-col space-y-2.5 p-4 md:-mt-32 text-gray-900">
+             
+                <h1>Bienvenido</h1>
+                <div className='link-start'>
+                    <Link href="/">COMENZAR</Link>
+                </div>
+            </div>
+        </main>
+    );
+}
